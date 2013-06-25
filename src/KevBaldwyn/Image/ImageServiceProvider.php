79c8d56ead28ad71116fca211194dda5a733fa38
Kevin Baldwyn <kev@kevbaldwyn.co.uk>
2013-06-25 17:49:02 +0100

    refactor service provider

--- src/KevBaldwyn/Image/ImageServiceProvider.php
+++ src/KevBaldwyn/Image/ImageServiceProvider.php
@@ -22,13 +22,27 @@ class ImageServiceProvider extends ServiceProvider {
 		
 		Config::package('kevbaldwyn/image', __DIR__.'/../../config');
 
-		$app = $this->app;
+		include(__DIR__.'/routes.php');
+
+		$this->registerWorker();
+		$this->registerCache();
+		$this->registerImage();
+
+	}
+
+
+	private function registerWorker() {
 
 		$this->app->bind('kevbaldwyn.image.worker', function() {
 			return \Imagecow\Image::create(Config::get('image::worker'));
 		});
 
-		$this->app->bind('kevbaldwyn.image.cache', function() use ($app) {
+	}
+
+
+	private function registerCache() {
+
+		$this->app->bind('kevbaldwyn.image.cache', function() {
             // default cache is file
             // trying to keep image cache separate from other cache
             $config = array();
@@ -38,15 +52,21 @@ class ImageServiceProvider extends ServiceProvider {
             return new \Illuminate\Cache\CacheManager($config);
 		});
 
+	}
+
+
+	private function registerImage() {
+
+		$app = $this->app;
+
 		$this->app->bind('kevbaldwyn.image', function() use ($app) {
 			return new \KevBaldwyn\Image\Image($app['kevbaldwyn.image.worker'], 
 											   $app['kevbaldwyn.image.cache'],
 											   Config::get('image::cache.lifetime'));
 		});
 
-		include(__DIR__.'/routes.php');
-
 	}
+
 
 	/**
 	 * Get the services provided by the provider.
