0a641072184da4e7694c9ec78f9180e1b0f6d009
Kevin Baldwyn <kev@kevbaldwyn.co.uk>
2014-05-22 11:18:48 +0100

    added test for path options

--- tests/ImageTest.php
+++ tests/ImageTest.php
@@ -20,9 +20,60 @@ class ImageTest extends \PHPUnit_Framework_TestCase {
 	}
 
 
+	public function testPathOptions()
+	{
+		$image = new Image(static::mockProvider(), 100, '/image-server');
+		// resize
+		$this->assertSame(
+			'/image-server?img=/path/to/image.jpg&transform=resize,400,200', 
+			$image->path('/path/to/image.jpg', 'resize', 400, 200)->__toString()
+		);
+		$this->assertSame(
+			'/image-server?img=/path/to/image.jpg&transform=resize,400,200,1', 
+			$image->path('/path/to/image.jpg', 'resize', 400, 200, 1)->__toString()
+		);
+		$this->assertSame(
+			'/image-server?img=/path/to/image.jpg&transform=resize,400', 
+			$image->path('/path/to/image.jpg', 'resize', 400)->__toString()
+		);
+
+		// resizeCrop
+		$this->assertSame(
+			'/image-server?img=/path/to/image.jpg&transform=resizeCrop,400,200', 
+			$image->path('/path/to/image.jpg', 'resizeCrop', 400, 200)->__toString()
+		);
+		$this->assertSame(
+			'/image-server?img=/path/to/image.jpg&transform=resizeCrop,400,200,center,middle', 
+			$image->path('/path/to/image.jpg', 'resizeCrop', 400, 200, 'center', 'middle')->__toString()
+		);
+
+		// crop
+		$this->assertSame(
+			'/image-server?img=/path/to/image.jpg&transform=crop,400,200', 
+			$image->path('/path/to/image.jpg', 'crop', 400, 200)->__toString()
+		);
+		$this->assertSame(
+			'/image-server?img=/path/to/image.jpg&transform=crop,400,200,left,top', 
+			$image->path('/path/to/image.jpg', 'crop', 400, 200, 'left', 'top')->__toString()
+		);
+		$this->assertSame(
+			'/image-server?img=/path/to/image.jpg&transform=crop,400,200,20,50%', 
+			$image->path('/path/to/image.jpg', 'crop', 400, 200, 20, '50%')->__toString()
+		);
+		$this->assertSame(
+			'/image-server?img=/path/to/image.jpg&transform=crop,50%,50%', 
+			$image->path('/path/to/image.jpg', 'crop', '50%', '50%')->__toString()
+		);
+	}
+
+
 	public static function mockProvider()
 	{
 		$provider = m::mock('\KevBaldwyn\Image\Providers\ProviderInterface');
+		$provider->shouldReceive('getVarImage')->andReturn('img');
+		$provider->shouldReceive('getVarTransform')->andReturn('transform');
+		$provider->shouldReceive('getVarResponsiveFlag')->andReturn('responsive');
+
 		return $provider;
 	}
 
